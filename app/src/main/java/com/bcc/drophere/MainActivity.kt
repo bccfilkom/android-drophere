4bd6a9d35d9dbb46460040aba05595aede61478b
adityapp <axceofficial@gmail.com>
2019-12-20 09:25:31 +0700

    init project
    changes :
     - create staging variant
     - add base url in string resource for manage endpoint

---
+++ app/src/main/java/com/bcc/drophere/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.bcc.drophere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("MainApplication", getString(R.string.base_url))
+    }
+}
